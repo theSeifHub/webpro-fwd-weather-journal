42342b6ea554f1347186927b71fa1a113f041cde
Seif Eldeen Ahmed <seifahmed870@yahoo.com>
2020-07-21 02:00:02 +0200

    server intitial setup

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
+"use strict";
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // Require Express to run server and routes
-
+const express = require("express");
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require("cors");
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static("website"));
 
 // Setup Server
+const port = 8888;
+const listeningSuccess = () => {
+  console.log(`Aahhh! I'm alive on ${port}`);
+};
+const server = app.listen(port, listeningSuccess);
+
+// post data function
+app.post("/add-weather", (req, res) => {
+  console.log(req.body.message); // DEBUGAAA
+  if (req.body.sys) {
+    console.log(`Server: Hello from ${req.body.name}, ${req.body.sys.country}!
+  #########################################`); // DEBUGAAA
+    console.log(req.body); // DEBUGAAA
+    const newCity = req.body;
+    projectData[`${req.body.id}`] = newCity;
+    res.send({
+      msg: "Data recieved",
+      data: projectData[`${req.body.id}`],
+    });
+  } else {
+    res.send({ msg: "other stuff happened" });
+  }
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,48 @@
 /* Global Variables */
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const apiKey = "&appid=78857c9b274884e72a9434418cabc940";
+
+/* Helper functions */
+
+// Get weather data from Open Weather API
+const getWeather = async (url, zip, key) => {
+  console.log(url + zip + key); // DEBUGAAA
+  let res = await fetch(`${url}${zip}${key}`);
+  try {
+    res = await res.json();
+    console.log("getWeather:\n" + res); // DEBUGAAA
+    return res;
+  } catch (error) {
+    console.log("HERE's an ERR man\n" + error); // DEBUGAAA
+    // appropriately handle the error
+  }
+};
+
+// Post received data to our express server
+const postDataToServer = async (url = "", data = {}) => {
+  let response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  response = await response.json();
+  return response;
+};
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+document.getElementById("generate").addEventListener("click", performAction);
+
+function performAction(e) {
+  const zipcode = document.getElementById("zip").value;
+  getWeather(baseURL, zipcode, apiKey)
+    .then((data) => postDataToServer("/add-weather", data))
+    .then((res) => console.log(res));
+}
+
+// https://worldpostalcode.com/
